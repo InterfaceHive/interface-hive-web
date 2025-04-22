58f8d9be0c1118eba20b1aa3a3913d089d21a867
Sunkyo Jung <sunkyo98@hotmail.com>
2025-04-21 19:49:41 -0500

    page init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ViewRequestsComponent } from './pages/view-requests/view-requests.component';
+import { CreateRequestComponent } from './pages/create-request/create-request.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'view-requests', component: ViewRequestsComponent },
+  { path: 'create-request', component: CreateRequestComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/create-request/create-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-request',
+  imports: [],
+  templateUrl: './create-request.component.html',
+  styleUrl: './create-request.component.css'
+})
+export class CreateRequestComponent {
+
+}
