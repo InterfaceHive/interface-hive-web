1f0da4bae000ee9b3e0a4218bcc59abc7f7dde80
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2025-04-25 23:09:07 -0500

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ViewRequestsComponent } from './features/view-requests/view-requests.component';
 import { CreateRequestComponent } from './features/create-request/create-request.component';
+import { ViewMyRequestsComponent } from './features/view-my-requests/view-my-requests.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'view-requests', component: ViewRequestsComponent },
   { path: 'create-request', component: CreateRequestComponent },
+  { path: 'view-my-requests', component: ViewMyRequestsComponent },
   { path: '**', redirectTo: '' }
 ];
