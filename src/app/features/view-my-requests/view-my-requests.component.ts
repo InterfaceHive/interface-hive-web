fdd2986b811e97c0c9e6e9548aaf8747b363b8d8
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2025-04-25 23:08:19 -0500

    view-my-request

---
+++ src/app/features/view-my-requests/view-my-requests.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-my-requests',
+  imports: [],
+  templateUrl: './view-my-requests.component.html',
+  styleUrl: './view-my-requests.component.css'
+})
+export class ViewMyRequestsComponent {
+
+}
