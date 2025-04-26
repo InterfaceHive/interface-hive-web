a8d8668bc7b96872c198f4413051e526f2a40bc7
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2025-04-26 00:03:03 -0500

    add view-my-contribution

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './features/home/home.component';
 import { ViewRequestsComponent } from './features/view-requests/view-requests.component';
 import { CreateRequestComponent } from './features/create-request/create-request.component';
 import { ViewMyRequestsComponent } from './features/view-my-requests/view-my-requests.component';
+import { ViewMyContributionsComponent } from './features/view-my-contributions/view-my-contributions.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'view-requests', component: ViewRequestsComponent },
   { path: 'create-request', component: CreateRequestComponent },
   { path: 'view-my-requests', component: ViewMyRequestsComponent },
+  { path: 'view-my-contributions', component: ViewMyContributionsComponent},
   { path: '**', redirectTo: '' }
 ];
