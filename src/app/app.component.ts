e5e086f86822623d31506cbd16ac9ced29bd89d4
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2025-04-25 20:36:12 -0500

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, NavbarComponent]
+  imports: [RouterOutlet, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'InterfaceHive';
