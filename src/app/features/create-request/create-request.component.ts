c3fc531d08ef43add34d40b9e600c0c6dae0c66b
Juyeong Yoon <jyoon79@wisc.edu>
2025-05-02 18:56:27 -0500

    Create request cretaed

--- src/app/features/create-request/create-request.component.ts
+++ src/app/features/create-request/create-request.component.ts
@@ -1,11 +1,34 @@
+// interfacehive-request.component.ts
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-create-request',
-  imports: [],
+  selector: 'app-interfacehive-request',
   templateUrl: './create-request.component.html',
-  styleUrl: './create-request.component.css'
+  styleUrls: ['./create-request.component.css']
 })
-export class CreateRequestComponent {
+export class InterfacehiveRequestComponent {
+  activeCodeTab: 'code-tab' | 'implementation-tab' = 'code-tab';
+  activeIOtab: 'input-tab' | 'output-tab' = 'input-tab';
+  tags: string[] = ['javascript', 'algorithm', 'sorting'];
+  newTag = '';
 
+  switchTab(tabGroup: 'code' | 'io', tabId: string) {
+    if (tabGroup === 'code') {
+      this.activeCodeTab = tabId as 'code-tab' | 'implementation-tab';
+    } else {
+      this.activeIOtab = tabId as 'input-tab' | 'output-tab';
+    }
+  }
+
+  addTag(event: KeyboardEvent) {
+    if (event.key === 'Enter' && this.newTag.trim()) {
+      event.preventDefault();
+      this.tags.push(this.newTag.trim());
+      this.newTag = '';
+    }
+  }
+
+  removeTag(index: number) {
+    this.tags.splice(index, 1);
+  }
 }
