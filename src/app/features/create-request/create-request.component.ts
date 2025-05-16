5eb93501a8388b93e3049a30e53aba9f8c713127
Sunkyo Jung <jung88@wisc.edu>
2025-05-16 04:46:15 -0500

    addTag/removeTag impl

--- src/app/features/create-request/create-request.component.ts
+++ src/app/features/create-request/create-request.component.ts
@@ -1,16 +1,29 @@
 // interfacehive-request.component.ts
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-interfacehive-request',
   templateUrl: './create-request.component.html',
-  styleUrls: ['./create-request.component.css']
+  styleUrls: ['./create-request.component.css'],
 })
 export class CreateRequestComponent {
   activeCodeTab: 'code-tab' | 'implementation-tab' = 'code-tab';
   activeIOtab: 'input-tab' | 'output-tab' = 'input-tab';
-  tags: string[] = ['javascript', 'algorithm', 'sorting'];
-  newTag = '';
+  // tags: string[] = ['javascript', 'algorithm', 'sorting'];
+  // newTag = '';
+
+  tags = new FormArray<FormControl<string>>([]);
+
+  addTag(tag: string) {
+    if(tag) {
+      this.tags.push(new FormControl(tag, {nonNullable: true}));
+    }
+  }
+
+  removeTag(index: number) {
+    this.tags.removeAt(index);
+  }
 
   switchTab(tabGroup: 'code' | 'io', tabId: string) {
     if (tabGroup === 'code') {
@@ -20,15 +33,15 @@ export class CreateRequestComponent {
     }
   }
 
-  addTag(event: KeyboardEvent) {
-    if (event.key === 'Enter' && this.newTag.trim()) {
-      event.preventDefault();
-      this.tags.push(this.newTag.trim());
-      this.newTag = '';
-    }
-  }
+  // addTag(event: KeyboardEvent) {
+  //   if (event.key === 'Enter' && this.newTag.trim()) {
+  //     event.preventDefault();
+  //     this.tags.push(this.newTag.trim());
+  //     this.newTag = '';
+  //   }
+  // }
 
-  removeTag(index: number) {
-    this.tags.splice(index, 1);
-  }
+  // removeTag(index: number) {
+  //   this.tags.splice(index, 1);
+  // }
 }
